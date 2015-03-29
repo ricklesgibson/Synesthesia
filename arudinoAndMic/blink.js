a8039f6b1c14248a3ad126d71d2380860eb38670
Rick Gibson <ricklesgibson@gmail.com>
2015-03-28 22:16:05 -0700

    working usb and mic

---
+++ arudinoAndMic/blink.js
@@ -0,0 +1,123 @@
+var connectionId = -1;
+var readBuffer = "";
+
+
+function setPosition(position) {
+  var buffer = new ArrayBuffer(1);
+  var uint8View = new Uint8Array(buffer);
+  uint8View[0] = position;
+  chrome.serial.send(connectionId, buffer, function() {});
+};
+
+
+function onOpen(openInfo) {
+  connectionId = openInfo.connectionId;
+  console.log("connectionId: " + connectionId);
+  if (connectionId == -1) {
+    setStatus('Could not open');
+    return;
+  }
+  setStatus('Connected');
+
+  setPosition(0);
+
+  chrome.serial.onReceive.addListener(function(data){
+    
+    var uint8View = new Uint8Array(data.data);
+    var value = String.fromCharCode(uint8View[1]);
+      // console.log("our data",data.data.byteLength);
+      console.log("uint8View", value);
+    if (value == "a") // Light on and off
+  {
+      console.log("CMD[a]: " + readBuffer);
+      var opat = isNaN(parseInt(readBuffer)) ? 0 : parseInt(readBuffer);
+      
+      document.getElementById('image').style.opacity = (opat* 0.7) + 0.3;
+      readBuffer = "";
+  }
+  else if (value == "b") // Return blink length value
+  {
+      readBuffer = "";
+  }
+  else if (value == "c") // Blink Count
+  {
+      console.log("CMD[c]: " + readBuffer);
+      document.getElementById('blinkCount').innerText = readBuffer;
+      readBuffer = "";
+  }
+  else
+  {
+    
+    readBuffer += value;
+  }
+
+  });
+};
+
+function setStatus(status) {
+  document.getElementById('status').innerText = status;
+}
+
+// original
+// function buildPortPicker(ports) {
+//   var eligiblePorts = ports.filter(function(port) {
+//     return !port.match(/[Bb]luetooth/) && port.match(/\/dev\/tty/);
+//   });
+
+// modified
+function buildPortPicker(ports) {
+  var eligiblePorts = ports;
+  
+
+  var portPicker = document.getElementById('port-picker');
+  eligiblePorts.forEach(function(port) {
+    var portOption = document.createElement('option');
+    portOption.value = portOption.innerText = port.path;
+    portPicker.appendChild(portOption);
+  });
+
+  portPicker.onchange = function() {
+    if (connectionId != -1) {
+      chrome.serial.disconnect(connectionId, openSelectedPort);
+      return;
+    }
+    openSelectedPort();
+  };
+}
+
+function openSelectedPort() {
+  var portPicker = document.getElementById('port-picker');
+  var selectedPort = portPicker.options[portPicker.selectedIndex].value;
+  console.log("selectedPort:", selectedPort);
+  chrome.serial.connect(selectedPort, onOpen);
+}
+
+window.onload = function() {
+
+  document.getElementById('position-input').onchange = function() {
+    setPosition(parseInt(this.value, 10));
+  };
+
+  window.AudioContext = window.AudioContext ||
+                        window.webkitAudioContext;
+
+  var context = new AudioContext();
+
+
+
+  // chrome.serial.getDevices(function(ports) {
+  //   console.log("hello", ports);
+  //   buildPortPicker(ports)
+  //   openSelectedPort();
+  // });
+
+
+  var onGetDevices = function(ports) {
+      buildPortPicker(ports);
+      openSelectedPort();
+    }
+
+  chrome.serial.getDevices(onGetDevices);
+
+
+};
---
+++ arudinoAndMic/turnonmic.js
@@ -0,0 +1,35 @@
+
+
+
+function toggleLiveInput() {
+    if (isPlaying) {
+        //stop playing and return
+        sourceNode.stop( 0 );
+        sourceNode = null;
+        analyser = null;
+        isPlaying = false;
+    if (!window.cancelAnimationFrame)
+      window.cancelAnimationFrame = window.webkitCancelAnimationFrame;
+        window.cancelAnimationFrame( rafID );
+    }
+    getUserMedia(
+      {
+            "audio": {
+                "mandatory": {
+                    "googEchoCancellation": "false",
+                    "googAutoGainControl": "false",
+                    "googNoiseSuppression": "false",
+                    "googHighpassFilter": "false"
+                },
+                "optional": []
+            },
+        }, gotStream);
+}
+
+// Add event listeners once the DOM has fully loaded by listening for the
+// `DOMContentLoaded` event on the document, and adding your listeners to
+// specific elements when it triggers.
+document.addEventListener('DOMContentLoaded', function () {
+  document.querySelector('button').addEventListener('click', toggleLiveInput);
+  main();
+});
